13b4b38d4bfcdeb8990e17528977bf459cb0fd55
amir <amirtaki623@gmail.com>
2025-04-29 01:57:07 +0330

    useId in react

--- src/App.js
+++ src/App.js
@@ -3,44 +3,20 @@ import { useMemo, useRef, useState } from 'react';
 import './App.css';
 
 
-
+import Input from './components/input/input';
 
 
 function App() {
   
-  const [number , setNumber] = useState (0);
-  // const addNumber = slowFunction(number)
-
-
-  const [theme, setTheme] = useState(false);
-
-  
-  // useMemo
-  const addNumber  = useMemo(() => slowFunction(number),[number])
-
-  // useMemo (function, [dependenci])
-
-  
   return (
-    <div className="App" style={{backgroundColor : theme ? "blue" : "white"}}>
-    
-    <h1>{addNumber}</h1>
-    <input type = 'text' onChange={(event) => {setNumber(event.target.value)}}/>
-  
-
-    <br />
-    <br />
-
-    {/* <button onClick={()=>(setTheme(theme ? false : true))}>Change them</button> */}
-    <button onClick={()=>(setTheme(!theme))}>Change them</button>
+    <div className="App">
+      <Input />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id necessitatibus vitae nisi laboriosam modi iure enim dolorum hic, consequatur recusandae architecto molestias optio debitis quos nihil omnis, blanditiis consequuntur dignissimos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id necessitatibus vitae nisi laboriosam modi iure enim dolorum hic, consequatur recusandae architecto molestias optio debitis quos nihil omnis, blanditiis consequuntur dignissimos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id necessitatibus vitae nisi laboriosam modi iure enim dolorum hic, consequatur recusandae architecto molestias optio debitis quos nihil omnis, blanditiis consequuntur dignissimos.</p>
+      <Input />
     </div>
   );
 }
 
 export default App;
-
-
-const slowFunction = (number) =>{
-  for (let i = 0;  i < 1000000000 ; i++){}
-  return number * 2
-}
