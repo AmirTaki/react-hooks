a402e130432c62094a594bd1f3df37c3816a4f34
amir <amirtaki623@gmail.com>
2025-04-29 02:02:15 +0330

    toturial ract hooks

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import { useMemo, useRef, useState } from 'react';
 import './App.css';
 
 
-import Input from './components/input/input';
-
 
 function App() {
   
   return (
     <div className="App">
-      <Input />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id necessitatibus vitae nisi laboriosam modi iure enim dolorum hic, consequatur recusandae architecto molestias optio debitis quos nihil omnis, blanditiis consequuntur dignissimos.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id necessitatibus vitae nisi laboriosam modi iure enim dolorum hic, consequatur recusandae architecto molestias optio debitis quos nihil omnis, blanditiis consequuntur dignissimos.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id necessitatibus vitae nisi laboriosam modi iure enim dolorum hic, consequatur recusandae architecto molestias optio debitis quos nihil omnis, blanditiis consequuntur dignissimos.</p>
-      <Input />
+    <h1>
+      toturial  React Hook 
+
+    </h1>
+
+    <h3>
+    13b4b38  ==> useId in react <hr/><hr/>
+    bca59d8  ==> useMemo in react <hr/><hr/>
+    353bf96  ==> useRef in react <hr/><hr/>
+    e5476d7  ==> useEffect in react <hr/><hr/>
+    6cf8cd8  ==> useState in react  <hr/><hr/>
+    f4dd781  ==> useContext in react <hr/><hr/>
+    ed6f062  ==> initial project    <hr/><hr/>
+    d5376db  ==> Initialize project using Create React App <hr/><hr/>
+    </h3>
     </div>
   );
 }
