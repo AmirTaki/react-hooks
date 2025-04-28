bca59d822b35620efc0edd72579e065d0c760162
amir <amirtaki623@gmail.com>
2025-04-29 01:36:01 +0330

    useMemo in react

--- src/App.js
+++ src/App.js
@@ -1,75 +1,46 @@
 
-import { useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import './App.css';
+
+
+
+
+
 function App() {
   
- const [count, setCount]  = useState(0);
-
- const countRef = useRef(0);
-
-//   // -------------------------------------------------
- 
-const handleIncrase = () =>{
-    setCount(count + 1);
-  }
-//   // -------------------------------------------------
-  const handleIncraseRef = () =>{
-    countRef.current ++;
-  }
-  console.log("render")
+  const [number , setNumber] = useState (0);
+  // const addNumber = slowFunction(number)
 
 
-    // -------------------------------------------------
-//  in DOM js
+  const [theme, setTheme] = useState(false);
 
-  // document.getElementById("#changeColor").style.color = "red"; ERROR
-
-  // ref 
-  const tagRef = useRef(null);
-
-  // function
-  const handleChangeColor = ()=>{
-    tagRef.current.style.color = "red";
-  }
-
-  // -------------------------------------------------
-  const inputRef = useRef(null);
-
-  const handleInput = () =>{
-    inputRef.current.focus()
-  }
-
-  return (
-    <div className="App">
- 
-      <h1>
-        state : {count}
-      </h1> 
-      <button onClick={handleIncrase}>Add State</button>
-          
-      <br />
-      <br />
-
-      <h1> ref : {countRef.current}</h1>
-      <button onClick={handleIncraseRef}>Add Ref</button>
-
-      <hr></hr>
-      <hr></hr>
-      <hr></hr>
-
-      {/* <h1 id = "changeColor">change color </h1> */}
-      
-      <h1 ref = {tagRef} onClick={handleChangeColor}>change color </h1>
-
-
-      <hr></hr>
-
-      <input ref = {inputRef} />
-
-      <button onClick={handleInput}>foucs</button>
   
+  // useMemo
+  const addNumber  = useMemo(() => slowFunction(number),[number])
+
+  // useMemo (function, [dependenci])
+
+  
+  return (
+    <div className="App" style={{backgroundColor : theme ? "blue" : "white"}}>
+    
+    <h1>{addNumber}</h1>
+    <input type = 'text' onChange={(event) => {setNumber(event.target.value)}}/>
+  
+
+    <br />
+    <br />
+
+    {/* <button onClick={()=>(setTheme(theme ? false : true))}>Change them</button> */}
+    <button onClick={()=>(setTheme(!theme))}>Change them</button>
     </div>
   );
 }
 
 export default App;
+
+
+const slowFunction = (number) =>{
+  for (let i = 0;  i < 1000000000 ; i++){}
+  return number * 2
+}
